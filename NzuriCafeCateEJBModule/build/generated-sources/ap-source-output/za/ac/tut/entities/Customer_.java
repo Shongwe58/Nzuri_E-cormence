c41ab97e6befcc46e495c15f621201edba735a6a
Mkhuleko Innocent <89659036+Shongwe58@users.noreply.github.com>
2025-05-17 15:15:03 +0200

    Add files via upload

---
+++ NzuriCafeCateEJBModule/build/generated-sources/ap-source-output/za/ac/tut/entities/Customer_.java
@@ -0,0 +1,18 @@
+package za.ac.tut.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-16T19:57:59")
+@StaticMetamodel(Customer.class)
+public class Customer_ { 
+
+    public static volatile SingularAttribute<Customer, String> password;
+    public static volatile SingularAttribute<Customer, String> surname;
+    public static volatile SingularAttribute<Customer, String> name;
+    public static volatile SingularAttribute<Customer, String> block;
+    public static volatile SingularAttribute<Customer, String> phone_number;
+    public static volatile SingularAttribute<Customer, String> id;
+
+}
---
+++ NzuriCafeCateEJBModule/src/java/za/ac/tut/entities/Customer.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author loveness
+ */
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    private String id;
+    private String name;
+    private String surname;
+    private String block;
+    private String phone_number;
+    private String password;
+
+    public Customer() {
+    }
+
+    public Customer(String id, String name, String surname, String block, String phone_number, String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.block = block;
+        this.phone_number = phone_number;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "za.ac.tut.entities.Customer[ id=" + id + " ]";
+    }
+    
+}
